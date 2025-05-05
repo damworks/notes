ebed46a815017a2fc77ecc3f62c9bf13e99c76f9
lchiarello <lchiarello@alpenite.com>
2025-05-05 08:30:45 +0200

    expose Note entity via ApiResource for API Platform

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\NoteRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NoteRepository::class)]
+#[ApiResource]
 class Note
 {
     #[ORM\Id]
